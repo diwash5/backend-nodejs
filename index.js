14926d16800f6902198d9f09b7087b60ffc1ab21
diwash5 <diwashsubediiii@gmail.com>
2022-12-09 22:15:41 +0545

    Mongoose Connected

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Importing express to run the web server for recieving and sending Json datas
 const express = require('express')
+// Importing MongoDB to deal with Database stuff
+const mongoose = require('mongoose')
 
-
+const mongooseurl = `${process.env.mongourl}`
 const app = express()
+// Dealing with some prompt
+mongoose.set('strictQuery', false);
 
 // send the response Hello World if anyone queries the home page
 app.get('/', (req, res ) => {
@@ -10,12 +14,13 @@ app.get('/', (req, res ) => {
 })
 
 // Making some routes so that everything must not be written in a single document
-
 const inputRoutes = require('./routes/input')
 
 app.use('/input', inputRoutes)
 
-
-
-//  Listen on App AKA run the Server
-app.listen(3600, () => console.log("I am started"))
+// Connect to mongoose and then Listen on App AKA run the Server
+mongoose.connect(mongooseurl)
+  .then((result) => {
+    app.listen(3600, () => console.log("I am started"))
+  })
+  .catch((err) => console.log(err));
