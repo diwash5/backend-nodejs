09e2ea9ecf66bf06c31507d22b00b6df196f4db7
diwash5 <diwashsubediiii@gmail.com>
2022-12-10 15:31:45 +0545

    Added Schema , Basic input is done , Started output but is unfinished and untested

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require('express')
 // Importing MongoDB to deal with Database stuff
 const mongoose = require('mongoose')
 
+
+// using environment variables from .vscode/launch.json
+// under configuration , env is added with mongoourl
+
+
 const mongooseurl = `${process.env.mongourl}`
+
+
 const app = express()
-// Dealing with some prompt
+// Dealing with some warning  prompt
 mongoose.set('strictQuery', false);
 
 // send the response Hello World if anyone queries the home page
@@ -15,8 +22,10 @@ app.get('/', (req, res ) => {
 
 // Making some routes so that everything must not be written in a single document
 const inputRoutes = require('./routes/input')
+const outputRoutes = require('./routes/output')
 
 app.use('/input', inputRoutes)
+app.use('/output', outputRoutes)
 
 // Connect to mongoose and then Listen on App AKA run the Server
 mongoose.connect(mongooseurl)
